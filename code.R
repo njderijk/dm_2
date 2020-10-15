5ff0ab9dd5ec0a3509852353449cb1956b1ea743
Rose Mary Hulscher <r.hulscher@intermax.nl>
2020-10-15 12:39:34 +0200

    added R code, made plots

--- code.R
+++ code.R
@@ -2,11 +2,124 @@ library(rpart.plot)
 library(tm)
 library(SnowballC)
 library(randomForest)
+library(tidyverse)
+library(quanteda)
+library(tidytext)
+library(qdap)
+library(ggplot2)
 
-txt_d <- Corpus(DirSource("~/deceptive/allfortm_d"), readerControl = list(language="lat")) #specifies the exact folder where my text file(s) is for analysis with tm.
-txt_t <- Corpus(DirSource("~/truthful/allfortm_t"), readerControl = list(language="lat"))
+# Create corpus
+# Negative deceptive 
+corpus_d <- Corpus(DirSource("./deceptive/allfortm_d"), readerControl = list(language="lat")) #specifies the exact folder where my text file(s) is for analysis with tm.
+# Negative truthful 
+corpus_t <- Corpus(DirSource("./truthful/allfortm_t"), readerControl = list(language="lat"))
 
-corpus <- Corpus(DirSource("~/all"), readerControl = list(language="lat"))
+#Negative truthful and deceptive both. 
+corpus <- Corpus(DirSource("./all"), readerControl = list(language="lat"))
+
+# Load the original data
+list_of_files_d <- list.files(path = "./deceptive/allfortm_d", recursive = TRUE,
+                              pattern = "*.txt", 
+                              full.names = TRUE)
+txt_d <- readtext(paste0(list_of_files_d))
+list_of_files_t <- list.files(path = "./truthful/allfortm_t", recursive = TRUE,
+                              pattern = "*.txt", 
+                              full.names = TRUE)
+txt_t <- readtext(paste0(list_of_files_t))
+
+# complete text
+txt_c <- bind_rows(txt_t, txt_d)
+labels <- c(rep("truthfull",320), rep("deceptive",320))
+
+# Complete text with labels [640x3]
+txt_c <- txt_c %>%
+  add_column(labels = factor(labels)) 
+
+stopwords <- tm::stopwords(kind = "en")
+
+# Prepare for word_cloud (no further preprocessing: needs to remove stopwords)
+dfm_corpus_t <- txt_c[txt_c$labels == "truthfull",] %>%
+  corpus(text_field = "text") %>%
+  tokens(remove_numbers = FALSE, remove_punct = TRUE, remove_symbols = TRUE, remove_separators = TRUE, split_hyphens	
+         = TRUE, remove_url = TRUE) %>% 
+  dfm()
+
+dfm_corpus_d <- txt_c[txt_c$labels == "deceptive",] %>%
+  corpus(text_field = "text") %>%
+  tokens(remove_numbers = FALSE, remove_punct = TRUE, remove_symbols = TRUE, remove_separators = TRUE, split_hyphens	
+ = TRUE, remove_url = TRUE) %>%
+  dfm() %>%
+  removeSparseTerms(0.995)
+
+# Textplot wordcloud
+set.seed(100)
+textplot_wordcloud(dfm_corpus_t, min_count = 100, random_order = FALSE,
+                   rotation = .25, 
+                   color = RColorBrewer::brewer.pal(8,"Dark2"))
+
+set.seed(100)
+textplot_wordcloud(dfm_corpus_d, min_count = 250, random_order = FALSE,
+                   rotation = .25, 
+                   color = RColorBrewer::brewer.pal(8,"Dark2"))
+
+### Tokenize the data (ALL THE DATA)
+cleaned_data_tokens <- txt_c %>% 
+  select(c("text", "labels", "doc_id")) %>% 
+  unnest_tokens(output = word, input = text)
+
+cleaned_data_tokens_nosw <- cleaned_data_tokens %>%
+  anti_join(get_stopwords())
+
+# Create dtm
+class_dtm_c <- cleaned_data_tokens_nosw %>%
+  count(doc_id, word) %>%
+  cast_dtm(document = doc_id, term = word, value = n)
+
+class_dtm_c
+
+# Reducing model complexity by removing sparse terms from the model: tokens that do not appear across many documents 
+class_dtm_c2 <- removeSparseTerms(class_dtm_c, sparse =.99) #97 terms 562
+class_dtm_c2
+# removeSparseTerms(class_dtm, sparse =.98) #96 terms 259
+# removeSparseTerms(class_dtm, sparse =.97) #97 terms 158
+
+# Plot the occurences of the word for each label
+type_words <-  cleaned_data_tokens_nosw %>%
+  count(labels, word, sort = TRUE)
+
+total_words <- type_words %>% 
+  group_by(labels) %>% 
+  summarize(total = sum(n))
+
+type_words <- left_join(type_words, total_words)
+type_words
+
+# Term frequency
+freq_by_rank <- type_words %>% 
+  group_by(labels) %>% 
+  mutate(rank = row_number(), 
+         tf = n/total)
+
+# plot the counts
+type_words %>%
+  group_by(labels) %>%
+  top_n(n=50,n) %>% 
+  ggplot(aes(x= word, y = n)) +
+  geom_col() +
+  labs(x = NULL, y = "n") +
+  facet_wrap(~ labels, scales = "free") +
+  coord_flip()
+
+# graph the top 50 tokens for 2 categories: plot the tf
+freq_by_rank %>%
+  top_n(50,tf) %>%
+  ungroup() %>%
+  ggplot(aes(word, tf)) +
+  geom_col() +
+  labs(x = NULL, y = "tf") +
+  facet_wrap(~ labels, scales = "free") +
+  coord_flip()
+
 
 # Process
 corpus <- tm_map(txt_all, removeNumbers)
